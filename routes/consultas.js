d885af93dfbc8af9d262e086ecc58bd46a51ab33
Ender <torresender96@gmail.com>
2024-02-12 23:26:36 -0300

    se agrego ejercicio 1 y 2

--- routes/consultas.js
+++ routes/consultas.js
@@ -3,9 +3,10 @@ const format = require ('pg-format');
 
 
 // CONSULTAS CON ORDENAMIENTO Y LIMITES
-const obtenerDatosOrdenamiento = async ({limit = 3, order_by = "stock_ASC"}) => {
+const obtenerDatosOrdenamiento = async ({limit = 3, order_by = "stock_ASC", page = 1}) => {
     const [campo,direccion] = order_by.split("_");
-    const formatQuery = format('SELECT * FROM inventario order by %s %s LIMIT %s', campo, direccion, limit);
+    const offset = (page - 1) * limit;
+    const formatQuery = format('SELECT * FROM inventario order by %s %s LIMIT %s OFFSET %s', campo, direccion, limit, offset);
 
     const {rows} = await pool.query(formatQuery);
     return rows
@@ -13,6 +14,71 @@ const obtenerDatosOrdenamiento = async ({limit = 3, order_by = "stock_ASC"}) => 
 
 
 
+const filtro = async ({ precio_min, precio_max, categoria, metal }) => {
+    let filtros = [];
+    let valores = [];
+    
+    if (precio_max) {
+        filtros.push(`precio <= $${valores.length + 1}`);
+        valores.push(precio_max);
+    }
+    if (precio_min) {
+        filtros.push(`precio >= $${valores.length + 1}`);
+        valores.push(precio_min);
+    }
+    if (categoria) {
+        filtros.push(`categoria = $${valores.length + 1}`);
+        valores.push(categoria);
+    }
+    if (metal) {
+        filtros.push(`metal = $${valores.length + 1}`);
+        valores.push(metal);
+    }
+
+    let consulta = "SELECT * FROM inventario";
+    if (filtros.length > 0) {
+        consulta += " WHERE " + filtros.join(" AND ");
+    }
+
+    const { rows } = await pool.query(consulta, valores);
+    return rows;
+};
+
+// const hateoas = (joyas) => {
+//     const results = joyas.map((joya) => {
+//         return {
+//             id: joya.id,
+//             name: joya.name,
+//             href: `http://localhost:3000/joyas/${joya.id}`,
+//         }
+//     }).slice(0, 5);
+//     const total = joyas.length;
+//     const hateoas = {
+//         total,
+//         results
+//     }
+//     return hateoas
+// }
+
+const HATEOAS = (joyas) => {
+    const results = joyas.map((joya) => {
+        return {
+             name: joya.nombre,
+             id: joya.id,
+             href: `joyas/${joya.id}`,
+        }
+    }).slice(0, 4)
+    const total = joyas.length
+    const HATEOAS = {
+    total,
+    results
+    }
+    return HATEOAS
+    }
+    
+
 module.exports = {
-    obtenerDatosOrdenamiento
+    obtenerDatosOrdenamiento,
+    filtro,
+    HATEOAS
 }
--- routes/index_routes.js
+++ routes/index_routes.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
-const  {obtenerDatosOrdenamiento} = require ('../routes/consultas.js')
+const  {obtenerDatosOrdenamiento,filtro,HATEOAS} = require ('../routes/consultas.js')
 
 router.get ('/joyas', async (req, res) => {
     try {
         const queryString = req.query;
         const resultados = await obtenerDatosOrdenamiento(queryString);
-        res.json(resultados);
+        const hateoas = await HATEOAS(resultados);
+        res.json(hateoas);
         
     } catch (error) {
         res.status(500).json({error: error.message})
  
     }
 }) 
+
+
+router.get ('/joyas/filtros', async (req, res) => {
+    try {
+        const queryString = req.query;
+        const resultados = await filtro(queryString);
+        res.json(resultados);
+        
+    } catch (error) {
+        res.status(500).json({error: error.message})
+ 
+    }
+})
 
 
 module.exports = router
