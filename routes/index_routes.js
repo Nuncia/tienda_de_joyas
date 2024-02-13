9649f0d4f713c690c8a35ab83d72c69997db237b
Monica <maaylahuerta@gmail.com>
2024-02-12 23:17:47 -0300

    se agregan metodo get ruta joyas/filtros y metodo obtenerJoyasFiltradas

--- routes/index_routes.js
+++ routes/index_routes.js
@@ -1,19 +1,64 @@
 const express = require('express');
 const router = express.Router();
 
-const  {obtenerDatosOrdenamiento} = require ('../routes/consultas.js')
+const {
+   obtenerDatosOrdenamiento,
+   obtenerJoyasFiltradas,
+} = require('../routes/consultas.js');
 
-router.get ('/joyas', async (req, res) => {
-    try {
-        const queryString = req.query;
-        const resultados = await obtenerDatosOrdenamiento(queryString);
-        res.json(resultados);
-        
-    } catch (error) {
-        res.status(500).json({error: error.message})
- 
-    }
-}) 
+router.get('/joyas', async (req, res) => {
+   try {
+      const queryString = req.query;
+      const resultados = await obtenerDatosOrdenamiento(queryString);
+      res.json(resultados);
+   } catch (error) {
+      res.status(500).json({ error: error.message });
+   }
+});
 
+router.get('/joyas/filtros', async (req, res) => {
+   try {
+      const queryString = req.query;
+      console.log(queryString);
+      const { rows: joyas } = await obtenerJoyasFiltradas(queryString);
+      const resultado = {
+         status: 'success request',
+         msg: 'Busqueda exitosa',
+         data: joyas,
+         error: false,
+      };
+      res.status(200).json(resultado);
+   } catch (error) {
+      res.status(500).json({ error: `Error interno: ${error}` });
+   }
+});
 
-module.exports = router
+const validarFiltros = (precio_max, precio_min, categoria, metal) => {
+   const precioMax = Number.parseInt(precio_max);
+   const precioMin = Number.parseInt(precio_min);
+   console.log(precioMax, precioMin, categoria, metal);
+   if (
+      typeof precioMax === 'number' &&
+      typeof precioMin === 'number' &&
+      typeof categoria === 'string' &&
+      typeof metal === 'string'
+   ) {
+      const respuesta = {
+         status: 'Datos validos',
+         msg: 'Tipos de datos validos',
+         datos: { precioMax, precioMin, categoria, metal },
+         error: false,
+      };
+      return respuesta;
+   } else {
+      const respuesta = {
+         status: 'Bad request',
+         msg: 'Tipo de dato incorrecto ',
+         datos: [],
+         error: true,
+      };
+      return respuesta;
+   }
+};
+
+module.exports = router;
